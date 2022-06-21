50cb2d9de5964f56f22ec8c7b84bcd5998a0763a
Guilherme Drumond <guilhermedrumond1978@gmail.com>
2022-06-20 22:58:59 -0300

    Começo do projeto

---
+++ src/components/highlightList/highlight/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+function Highlight() {
+    return(
+        <div className="highlight-wrapper">
+            <div  className="text-content">
+<h3 className="highlight-title">Lorem Ipsum</h3>
+<h4 className="highlight-subtitle">Vivamus condimentum, diam quis varius pellentesque, quam eros egestas massa, at sodales ex libero commodo odio.</h4>
+            </div>
+            <div className="fake-image">
+
+            </div>
+        </div>
+    )
+}
+
+export default Highlight
---
+++ src/components/sideNews/index.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+
+function SideNews() {
+    return (
+        <div className="side-news">
+            <h2 className="news-title">Lorem Ipsum</h2>
+            <div>
+                <p className="news-text">
+                    Mauris blandit mi a mauris dignissim, sed auctor mauris vestibulum. Integer iaculis pharetra sapien vitae tincidunt. Quisque ultricies nulla non nisl mattis, a dignissim elit posuere. Quisque ultrices nisi sit amet nulla eleifend rutrum. Donec dignissim egestas odio a viverra. Sed lectus libero, lacinia in feugiat a, viverra non nibh. Sed non elementum massa.
+                </p>
+                <p className="news-text">
+                    Duis consectetur lorem urna, vel iaculis nibh consectetur non. Sed nec eros porta, congue risus vel, scelerisque arcu. Duis blandit nisi sit amet lacus sagittis pretium. Donec pretium posuere ante, a placerat tortor viverra eu. Etiam porttitor condimentum sollicitudin.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default SideNews
